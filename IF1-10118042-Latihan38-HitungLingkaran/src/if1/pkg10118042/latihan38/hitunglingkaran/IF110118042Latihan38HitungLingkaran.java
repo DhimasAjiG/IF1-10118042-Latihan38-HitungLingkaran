27dd96332900ecc1d6f3157009396237166c2dbd
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-10-17 13:21:12 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan38-HitungLingkaran/src/if1/pkg10118042/latihan38/hitunglingkaran/IF110118042Latihan38HitungLingkaran.java
@@ -0,0 +1,23 @@
+
+package if1.pkg10118042.latihan38.hitunglingkaran;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk mencari jari-jari, keliling, dan luas lingkaran
+ * 
+ */
+public class IF110118042Latihan38HitungLingkaran {
+
+   
+    public static void main(String[] args) {
+        Lingkaran o = new Lingkaran();
+       
+        System.out.println("======Perhitungan Lingkaran======");
+        o.validasi();
+        o.tampil(o.diameter);
+    }
+
+}
